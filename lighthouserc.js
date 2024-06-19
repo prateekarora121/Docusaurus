59a79a89820c2409ec62f683223cae147a2877b7
Parteek Arora <51459022+prateekarora121@users.noreply.github.com>
2024-06-19 20:27:38 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,17 @@ module.exports = {
     },
     collect:{
       staticDistDir:'website/build',
-      staticDirFileDiscoveryDepth:10
+      staticDirFileDiscoveryDepth:10,
+      numberOfRuns: 1
+    },
+    server:{
+      port:9001
+    },
+    upload:{
+      target: 'lhci',
+      serverBaseUrl: 'http://localhost:9001',
+      token:"1846a2d4-259a-4f2d-88c4-8040782b967d"
     }
+
   },
 };
